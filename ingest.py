66cae3f3fe8504bd0fcb4bb4517abd929f9eb50a
Steven Bamford <steven@stevenbamford.com>
2023-05-21 01:03:49 +0100

    Scripts to perform ingest of data for each night in separate qsub job

---
+++ ingest.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from tqdm import tqdm
+from functools import partialmethod
+
+from weaveio.file import File
+from weaveio.opr3 import Data
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+
+tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
+
+
+def match_files(filetype, directory, night):
+    """Returns all matching files within a directory"""
+    return (f for f in Path(directory).rglob(f'*{night}/*.fit*') if filetype.match_file(directory, f, None))
+
+
+def find_files(data, night):
+    filelist = []
+    for filetype in data.filetypes:
+        filelist += sorted([i for i in match_files(filetype, data.rootdir, night)], key=lambda f: f.name)
+    return [f for f in filelist if File.check_mos(f)]
+
+
+def main(night):
+    data = Data()
+    logger.info(f'Preparing to ingest data for night {night}')
+    with data.write:
+        fs = find_files(data, night)
+        if len(fs) == 0:
+            logger.info(f'No MOS files found')
+        else:
+            data.write_files(*fs, timeout=10*60, debug=False, test_one=False, debug_time=False,
+                             debug_params=False, dryrun=False)
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1])
---
+++ ingest.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -Tf
+#PBS -N WL-INGEST
+#PBS -l nodes=1
+#PBS -l walltime=06:00:00
+#PBS -l pmem=6gb
+#PBS -k oe
+#PBS -j oe
+#PBS -v PATH,WEAVEIO_USER,WEAVEIO_PASSWORD
+
+# This should be run as:
+# qsub -V -v NIGHT=20170226 ingest.sh
+# or to submit jobs for lots of nights:
+# ls $WEAVEIO_ROOTDIR/raw/ | grep -v txt | xargs -I{} qsub -V -v NIGHT={} ingest.sh
+
+export WEAVEIO_DB=lorentz
+
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo $PATH
+SAVED_WEAVEIO_USER=$WEAVEIO_USER
+SAVED_WEAVEIO_PASSWORD=$WEAVEIO_PASSWORD
+export PYTHONUNBUFFERED=1
+cd /home2/bamford/weave-io
+echo Starting ingest `date`
+echo Ingesting night $NIGHT to DB $WEAVEIO_DB
+conda run -n weaveio WEAVEIO_USER=$SAVED_WEAVEIO_USER WEAVEIO_PASSWORD=$SAVED_WEAVEIO_PASSWORD python ingest.py $NIGHT
+echo ------------------------------------------------------
+echo Job ends `date`
